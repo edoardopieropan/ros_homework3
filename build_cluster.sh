527f77a956d330f116ac379a80d3dab50f51aec5
Edoardo Pieropan <edoardo.pieropan@studenti.univr.it>
2018-06-10 11:58:58 +0200

    removed

--- build_cluster.sh
+++
@@ -1,6 +0,0 @@
-cd Cluster/
-mkdir build
-cd build
-cmake ..
-cmake --build .
-mv cluster_extraction ../../
--- run.sh
+++
@@ -1,9 +0,0 @@
-gnome-terminal -e "roscore"
-
-sleep 3
-
-export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/ros_homework3/ORB_SLAM2_mod/Examples/ROS
-
-gnome-terminal -e "rosrun ORB_SLAM2 Stereo /home/edoardo/catkin_ws/src/git/ORB_SLAM2/Vocabulary/ORBvoc.txt /home/edoardo/catkin_ws/src/git/ORB_SLAM2/Examples/Stereo/EuRoC.yaml true"
-
-gnome-terminal -e "rosbag play --pause V1_01_easy.bag /cam0/image_raw:=/camera/left/image_raw /cam1/image_raw:=/camera/right/image_raw"
