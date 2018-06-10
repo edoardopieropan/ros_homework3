8727da2d3d873cb90cfe6ad7b1bdc6baaacaafad
Edoardo Pieropan <edoardo.pieropan@studenti.univr.it>
2018-06-10 13:42:00 +0200

    Update System.h

--- ORB_SLAM2/include/System.h
+++ ORB_SLAM2/include/System.h
@@ -112,9 +112,10 @@ public:
     // See format details at: http://www.cvlibs.net/datasets/kitti/eval_odometry.php
     void SaveTrajectoryKITTI(const string &filename);
 
-    // Save the map points
+  
+    // Save the pointcloud map
     void CreatePCD(const string &filename);
-
+    
     // TODO: Save/Load functions
     // SaveMap(const string &filename);
     // LoadMap(const string &filename);
